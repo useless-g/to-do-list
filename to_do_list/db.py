80a356800b902aa91e5e40cfd69bf27780c8e249
Sergey <uzellessmovic@gmail.com>
2022-07-23 00:01:01 +0300

    fix creating db

--- to_do_list/db.py
+++ to_do_list/db.py
@@ -1,5 +1,8 @@
 from databases import Database
 from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+from .model import metadata
 
 TESTING = False
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/to_do"
@@ -10,3 +13,7 @@ if TESTING:
 
 Base = Database(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)  # echo all SQL in stdout
+
+if not database_exists(SQLALCHEMY_DATABASE_URL):
+    create_database(SQLALCHEMY_DATABASE_URL)  # Create the database.
+metadata.create_all(engine)
